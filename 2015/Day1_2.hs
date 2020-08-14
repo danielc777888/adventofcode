b1f5985f94ec3eff9eaab480b791aa8ee7281bd5
daniel.cabral <danielc777888@gmail.com>
2020-08-14 18:49:02 +0200

    complete day1_2

---
+++ 2015/Day1_2.hs
@@ -0,0 +1,9 @@
+solve :: String -> Int
+solve = length . head . dropWhile (\x -> (sum x) >= 0) . reverse . inits . map (\x -> if x == '(' then 1 else -1)
+
+inits :: [a] -> [[a]]
+inits [x] = [[x]]
+inits xs = [xs] ++ inits (init xs)
+
+main :: IO()
+main = interact (show . solve)
