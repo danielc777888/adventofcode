528df37ea9104b16cda35d07c4dd9996e43c27a5
daniel.cabral <danielc777888@gmail.com>
2020-10-26 20:38:22 +0200

    2015:3:2:COMPLETED

---
+++ 2015/Day3_2.hs
@@ -0,0 +1,51 @@
+
+
+main :: IO()
+main = interact solve
+
+solve :: String -> String
+solve xs = show . length . remdups . qsort $ simulate (roboSantaInstructions xs) ++  simulate (santaInstructions xs)
+--solve xs = show .  remdups . qsort $ simulate (roboSantaInstructions xs)
+
+
+data Direction = North | South | East | West
+
+type Location = (Int, Int)
+
+type Locations = [Location]
+
+direction :: Char -> Direction
+direction '^' = North
+direction 'v' = South
+direction '>' = East
+direction '<' = West
+
+santaInstructions :: String -> String
+santaInstructions xs = map snd ( filter (odd . fst) (zip [1..] xs))
+
+roboSantaInstructions :: String -> String
+roboSantaInstructions xs = map snd ( filter (even . fst) (zip [1..] xs))
+
+
+simulate :: String -> Locations
+simulate  = remdups . qsort . foldr (\x (y:ys) -> move y (direction x) : y : ys) [(0,0)] . reverse
+
+move :: Location -> Direction -> Location
+move (x, y) North = (x, y + 1)
+move (x, y) South = (x, y - 1)
+move (x, y) East = (x + 1, y)
+move (x, y) West = (x - 1, y)
+
+qsort :: Locations -> Locations
+qsort [] = []
+qsort [x] = [x]
+qsort (x:xs) = sortp xs [] []
+  where sortp [] us vs = qsort us ++ [x] ++ qsort vs
+        sortp (y:xs) us vs = if (fst y < fst x || snd y < snd x)
+                             then sortp xs (y:us) vs
+                             else sortp xs us (y:vs)
+
+
+remdups :: Locations -> Locations
+remdups [] = []
+remdups (x:xs) = x:remdups (dropWhile (==x) xs)
