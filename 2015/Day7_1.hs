c8d86b021748f686e560f23b9f614d44da1fc2a2
daniel.cabral <danielc777888@gmail.com>
2020-12-01 09:11:41 +0200

    2015:7:PENDING

--- 2015/Day7_1.hs
+++ 2015/Day7_1.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Bits
 
 main :: IO()
 main = interact solve
@@ -19,39 +20,48 @@ instruction xs
   | otherwise = toSignal ws
   where ws = words xs
 
+
+blank :: Int
+blank = (-1)
+
 toNot :: [String] -> Instruction
-toNot xs = GateInstruction (Not (xs!!1, 0)) (xs!!3, 0) --NOT x -> h
+toNot xs = GateInstruction (Not (xs!!1, blank)) (xs!!3, blank) --NOT x -> h
 
 toAnd :: [String] -> Instruction
-toAnd xs = GateInstruction (And (xs!!0, 0) (xs!!2, 0)) (xs!!4, 0) --x AND y -> d
+toAnd xs = GateInstruction (And (xs!!0, blank) (xs!!2, blank)) (xs!!4, blank) --x AND y -> d
 
 toOr :: [String] -> Instruction
-toOr xs = GateInstruction (Or (xs!!0, 0) (xs!!2, 0)) (xs!!4, 0) --x OR y -> d
+toOr xs = GateInstruction (Or (xs!!0, blank) (xs!!2, blank)) (xs!!4, blank) --x OR y -> d
 
 toLShift :: [String] -> Instruction
-toLShift xs = GateInstruction (LShift (xs!!0, 0) (toInt (xs!!2))) (xs!!4, 0)  --x LSHIFT 2 -> f
+toLShift xs = GateInstruction (LShift (xs!!0, blank) (toInt (xs!!2))) (xs!!4, blank)  --x LSHIFT 2 -> f
 
 toRShift :: [String] -> Instruction
-toRShift xs = GateInstruction (RShift (xs!!0, 0) (toInt (xs!!2))) (xs!!4, 0)  --x RSHIFT 2 -> f
+toRShift xs = GateInstruction (RShift (xs!!0, blank) (toInt (xs!!2))) (xs!!4, blank)  --x RSHIFT 2 -> f
   
 toSignal :: [String] -> Instruction
-toSignal xs = SignalInstruction ( toInt (xs!!0))  (xs!!2, toInt (xs!!0)) --123 -> x
+toSignal xs = SignalInstruction ( toInt (xs!!blank))  (xs!!2, toInt (xs!!blank)) --123 -> x
 
 
 toInt :: String -> Int
 toInt xs = (read xs) :: Int
 
 showWires :: [Wire] -> String
-showWires = undefined
+showWires = unlines . map (\x -> (fst x) ++ ":" ++ show (snd x))
 
-data Instruction = GateInstruction Gate Wire
-                 | SignalInstruction Signal Wire deriving (Eq, Show)
+runGate :: Gate -> Signal
+runGate (And (_, x) (_, y)) = x .&. y 
+runGate _ = error "cannot run gate"
+
+
+data Instruction = SignalInstruction Signal Wire 
+                 | GateInstruction Gate Wire deriving (Eq, Ord, Show)
 
 data Gate = And Wire Wire
             | Or Wire Wire
             | LShift Wire Int
             | RShift Wire Int
-            | Not Wire deriving (Eq, Show)
+            | Not Wire deriving (Eq, Show, Ord)
 
 type Wire = (Identifier, Signal)
 type Signal = Int
