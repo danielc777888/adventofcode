f3c14c395f33b2dee2af62818298b88f456a277f
daniel.cabral <danielc777888@gmail.com>
2020-12-01 09:36:36 +0200

    2020:1:COMPLETED

---
+++ 2020/Day1_1.hs
@@ -0,0 +1,11 @@
+
+main :: IO()
+main  = interact solve
+
+solve :: String -> String
+solve =  show . solve' . map (\x -> read x :: Int) .  lines
+
+
+solve' :: [Int] -> Int
+solve' xs = head [x * y | x <- xs, y <- xs, y + x == 2020]
+
---
+++ 2020/Day1_2.hs
@@ -0,0 +1,10 @@
+
+main :: IO()
+main  = interact solve
+
+solve :: String -> String
+solve =  show . solve' . map (\x -> read x :: Int) .  lines
+
+solve' :: [Int] -> Int
+solve' xs = head [x * y * z | x <- xs, y <- xs, z <- xs,  y + x + z == 2020]
+
