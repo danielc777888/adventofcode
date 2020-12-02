90917d8b279b6caa5dc8af09eba50b3d2f3555c1
daniel.cabral <danielc777888@gmail.com>
2020-12-02 19:04:28 +0200

    2015:7:PENDING

--- 2015/Day7_1.hs
+++ 2015/Day7_1.hs
@@ -1,23 +1,67 @@
 import Data.List
 import Data.Bits
+import Data.Char
 
 main :: IO()
 main = interact solve
 
 solve :: String -> String
-solve = showWires . run . map instruction . lines
+solve = showWires . sort . run . sort . map instruction . lines
 
 run :: [Instruction] -> [Wire]
-run = undefined
+run xs = if all resolved xs then wires else run (updateInstructions xs wires)
+  where wires = filter (\x -> snd x /= blank) (map wire xs)
 
+resolved :: Instruction -> Bool
+resolved (SignalInstruction _) = True
+resolved (GateInstruction _ (_, y)) = y /= blank
+resolved (WireInstruction _ (_, y)) = y /= blank
+
+wire :: Instruction -> Wire
+wire (SignalInstruction w) = w
+wire (GateInstruction _ w) = w
+wire (WireInstruction _ w) = w
+
+signalsProvided :: Gate -> Bool
+signalsProvided (And (_, x) (_, y)) = x /= blank && y /= blank
+signalsProvided (Or (_, x) (_, y)) = x /= blank && y /= blank
+signalsProvided (LShift (_, x) _ ) = x /= blank
+signalsProvided (RShift (_, x) _ ) = x /= blank
+signalsProvided (Not (_, x)) = x /= blank
+
+runInstruction :: Instruction -> Instruction
+runInstruction (SignalInstruction (x, y)) = SignalInstruction (x, y)
+runInstruction (GateInstruction g (x, y)) = GateInstruction g (x, if signalsProvided g then runGate g else y)
+runInstruction (WireInstruction (x, y) (x', y')) = WireInstruction (x, y) (x', y)
+
+updateInstructions :: [Instruction] -> [Wire] -> [Instruction]
+updateInstructions xs ws = map (\i -> if resolved i then i else runInstruction (updateInstruction' i ws)) xs
+
+updateInstruction' :: Instruction -> [Wire] -> Instruction
+updateInstruction' i [] = i
+updateInstruction' i (x:xs) = if resolved ui then ui else updateInstruction' ui xs
+  where ui = updateInstruction i x
+
+updateInstruction :: Instruction -> Wire -> Instruction
+updateInstruction (SignalInstruction (x, y)) _ = SignalInstruction (x, y)
+updateInstruction (GateInstruction (And w1 w2) w3 ) w4  = GateInstruction (And (updateWire w1 w4) (updateWire w2 w4)) w3
+updateInstruction (GateInstruction (Or w1 w2) w3 ) w4  = GateInstruction (Or (updateWire w1 w4) (updateWire w2 w4)) w3
+updateInstruction (GateInstruction (LShift w1 x) w3 ) w4  = GateInstruction (LShift (updateWire w1 w4) x) w3
+updateInstruction (GateInstruction (RShift w1 x) w3 ) w4  = GateInstruction (RShift (updateWire w1 w4) x) w3
+updateInstruction (GateInstruction (Not w1) w3 ) w4  = GateInstruction (Not (updateWire w1 w4) ) w3
+updateInstruction (WireInstruction w1 w2) w3 = WireInstruction (updateWire w1 w3) w2
+
+updateWire :: Wire -> Wire -> Wire
+updateWire (x, y) (x', y') = if x == x' then (x, y') else (x, y) 
+                                                                                             
 instruction :: String -> Instruction
 instruction xs
   | "NOT" `isInfixOf` xs = toNot ws
   | "AND" `isInfixOf` xs = toAnd ws
   | "OR" `isInfixOf` xs = toOr ws
-  | "LSFHIFT" `isInfixOf` xs = toLShift ws
+  | "LSHIFT" `isInfixOf` xs = toLShift ws
   | "RSHIFT" `isInfixOf` xs = toRShift ws
-  | otherwise = toSignal ws
+  | otherwise = if isDigit (xs!!0) then toSignal ws else toWire ws
   where ws = words xs
 
 
@@ -40,7 +84,10 @@ toRShift :: [String] -> Instruction
 toRShift xs = GateInstruction (RShift (xs!!0, blank) (toInt (xs!!2))) (xs!!4, blank)  --x RSHIFT 2 -> f
   
 toSignal :: [String] -> Instruction
-toSignal xs = SignalInstruction ( toInt (xs!!blank))  (xs!!2, toInt (xs!!blank)) --123 -> x
+toSignal xs = SignalInstruction (xs!!2, toInt (xs!!0)) --123 -> x
+
+toWire :: [String] -> Instruction
+toWire xs = WireInstruction (xs!!0, blank) (xs!!2, blank)  --lx -> x
 
 
 toInt :: String -> Int
@@ -50,12 +97,16 @@ showWires :: [Wire] -> String
 showWires = unlines . map (\x -> (fst x) ++ ":" ++ show (snd x))
 
 runGate :: Gate -> Signal
-runGate (And (_, x) (_, y)) = x .&. y 
-runGate _ = error "cannot run gate"
+runGate (And (_, x) (_, y)) = x .&. y
+runGate (Or (_, x) (_, y)) = x .|. y
+runGate (LShift (_, x) y) = shiftL x y
+runGate (RShift (_, x) y) = shiftR x y 
+runGate (Not (_, x)) = 65535 - x 
 
 
-data Instruction = SignalInstruction Signal Wire 
-                 | GateInstruction Gate Wire deriving (Eq, Ord, Show)
+data Instruction = SignalInstruction Wire 
+                 | GateInstruction Gate Wire
+                 | WireInstruction Wire Wire deriving (Eq, Ord, Show)
 
 data Gate = And Wire Wire
             | Or Wire Wire
