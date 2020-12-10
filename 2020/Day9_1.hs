7fd3af083544627e50c5805c3b5438d9e80e0a9f
daniel.cabral <danielc777888@gmail.com>
2020-12-10 12:38:25 +0200

    day 9 finit

---
+++ 2020/Day9_1.hs
@@ -0,0 +1,17 @@
+
+
+main :: IO()
+main = interact solve
+
+solve :: String -> String
+solve = show . head . filter(\(_, y) -> y == False) . check 25 . map (\x -> read x::Int) . lines
+
+check :: Int -> [Int] -> [(Int, Bool)]
+check n xs  = check' n (drop n xs) (take n xs) xs
+
+check' :: Int -> [Int] -> [Int] -> [Int] -> [(Int, Bool)]
+check' _ [] _ _ = []
+check' n (x:xs) ys (z:zs) = (x, valid x ys):check' n xs (take n zs) zs 
+
+valid :: Int -> [Int] -> Bool
+valid n xs = [x+y | x <- xs, y <- xs, x /= y && (x + y) == n] /= []
---
+++ 2020/Day9_2.hs
@@ -0,0 +1,32 @@
+import Data.List
+
+main :: IO()
+main = interact solve
+
+solve :: String -> String
+solve xs = show $ cont n xs'
+  where xs' = map (\x -> read x::Int) $ lines xs
+        ys = check 25 xs'
+        n = fst $ head $ filter(\(_, y) -> y == False) ys
+
+check :: Int -> [Int] -> [(Int, Bool)]
+check n xs  = check' n (drop n xs) (take n xs) xs
+
+check' :: Int -> [Int] -> [Int] -> [Int] -> [(Int, Bool)]
+check' _ [] _ _ = []
+check' n (x:xs) ys (z:zs) = (x, valid x ys):check' n xs (take n zs) zs 
+
+valid :: Int -> [Int] -> Bool
+valid n xs = [x+y | x <- xs, y <- xs, x /= y && (x + y) == n] /= []
+
+cont :: Int -> [Int] -> Int
+cont n xs = s + l
+  where xs' = cont' n (filter(<n) xs)
+        s = minimum xs'
+        l = maximum xs'
+
+cont' :: Int -> [Int] -> [Int]
+cont' _ [] = []
+cont' n (x:xs) = ys ++ cont' n xs
+  where xs' = inits (x:xs)
+        ys = concat $ filter (\xs -> sum xs == n) xs'
