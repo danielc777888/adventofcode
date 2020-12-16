b13ca931e3df47a14691c9b03407c669016fcfad
daniel.cabral <danielc777888@gmail.com>
2020-12-16 11:29:50 +0200

    complete day 13. 2nd part took 1.5 hours to get solution.

--- 2020/Day13_2.hs
+++ 2020/Day13_2.hs
@@ -1,17 +1,22 @@
 main :: IO()
 main = interact solve
 
-type Time = Integer
-type BusID = Integer
+type Time = Int
+type BusID = Int
 
-noRestrictions :: Integer
+noRestrictions :: Int
 noRestrictions = 1
 
-timeLimit :: Integer
-timeLimit = 109000000000000
-
-start :: Integer
-start = 99999999999984
+timeLimit :: Int
+--timeLimit = 200000000000000
+--timeLimit = 101100000000000 --67s old alg
+--timeLimit = 101100000000000 --14 new algo alg and Int
+--timeLimit = 500000000000000
+timeLimit = 1000000001000000
+start :: Int
+--start = 99999999961000 --multiple of largest num 787
+--start = 299999999993180
+start = 0
 
 
 solve :: String -> String
@@ -29,14 +34,17 @@ busIds xs = if x == "x" then noRestrictions:busIds xs' else read x:busIds xs'
              xs' = if null y then [] else tail y
                  
 earliest :: [BusID] -> Time
---earliest xs = head [t | t <- [0,(head xs)..timeLimit], match t xs]
-earliest xs = head [t-ll | t <- [start, (start + mb)..timeLimit], match xs (t-ll)]
+earliest xs = head [t-ll | t <- [start, (start + mb)..timeLimit], match rs (t-ll)]
   where mb = maximum xs
         (l, r) = break (==mb) xs
-        ll = toInteger (length l)
+        ll = length l
+        rs = runs xs
  
-match :: [BusID] -> Time -> Bool
-match [] t = True
-match (x:xs) t = if t `mod` x == 0 || x == noRestrictions then match xs (t+1) else False
+match :: [(BusID, Int)] -> Time -> Bool
+match [(x, y)] t = t `mod` x == 0
+match ((x, y):xs) t = if t `mod` x == 0 then match xs (t+y) else False
 
-
+runs :: [BusID] -> [(BusID, Int)]
+runs [x] = [(x, 0)]
+runs (x:xs) = (x, r):runs (dropWhile (==noRestrictions) xs)
+  where r = length (takeWhile (==noRestrictions) xs) + 1
