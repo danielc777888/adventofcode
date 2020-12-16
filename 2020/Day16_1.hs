75ec3495a0f66e3bbf8000b857f60a5e5e0fbe74
daniel.cabral <danielc777888@gmail.com>
2020-12-16 17:05:29 +0200

    Day 16 completed. Alright!

---
+++ 2020/Day16_1.hs
@@ -0,0 +1,43 @@
+import Data.List
+
+main :: IO()
+main = interact solve
+
+type Rule = (Int, Int)
+
+solve :: String -> String
+solve = show . sum . invalids . notes . lines
+
+notes :: [String] -> ([Rule], [Int])
+notes xs = (rules xs, nearbys xs)
+
+rules :: [String] -> [Rule]
+rules [] = []
+rules (x:xs)
+  | ":" `isInfixOf` x = rule xs' ++ rules xs
+  | otherwise = []
+  where xs' = words (drop 2 (snd (break (==':') x)))
+
+rule :: [String] -> [Rule]
+rule xs = [rule' (xs!!0)] ++ [rule' (xs!!2)]
+
+rule' :: String -> Rule
+rule' xs = (read l, read (tail r))
+  where (l, r) = break (=='-') xs
+          
+nearbys :: [String] -> [Int]
+nearbys [] = []
+nearbys (x:xs)
+  | "nearby" `isPrefixOf` x = nearby (intercalate "," xs)
+  | otherwise = nearbys xs
+
+nearby :: String -> [Int]
+nearby [] = []
+nearby (x:xs) = read l:if null r then [] else nearby (tail r)
+  where (l, r) = break (==',') (x:xs)
+
+invalids :: ([Rule], [Int]) -> [Int]
+invalids (xs, ys) = [y | y <- ys , not (valid xs y)]
+
+valid :: [Rule] -> Int -> Bool
+valid xs x' = any (\(x, y) -> x' <= y && x' >= x) xs
---
+++ 2020/Day16_2.hs
@@ -0,0 +1,80 @@
+import Data.List
+
+main :: IO()
+main = interact solve
+
+type Ticket = [Int]
+type Positions = [(Position, Field)]
+type Field = (Name, [Rule])
+type Rule = (Int, Int)
+type Name = String
+type Position = Int
+
+solve :: String -> String
+solve xs = show (product (fieldValues "departure" (remdups ps []) yt))
+  where ls = lines xs
+        fs = fields ls
+        yt = yours ls
+        nt = nearbys ls
+        rs = concat (map snd fs)
+        nt' = discard rs nt
+        ps = validPositions fs (transpose ([yt] ++ nt'))
+
+fields :: [String] -> [Field]
+fields [] = []
+fields (x:xs)
+  | ":" `isInfixOf` x = [(l, rule xs')] ++ fields xs
+  | otherwise = []
+  where (l, r) = break (==':') x
+        xs' = words (drop 2 r)
+
+rule :: [String] -> [Rule]
+rule xs = [rule' (xs!!0)] ++ [rule' (xs!!2)]
+
+rule' :: String -> Rule
+rule' xs = (read l, read (tail r))
+  where (l, r) = break (=='-') xs
+
+yours :: [String] -> Ticket
+yours [] = []
+yours  (x:y:xs)
+  | "your" `isPrefixOf` x = ticket y
+  | otherwise = yours (y:xs)
+          
+nearbys :: [String] -> [Ticket]
+nearbys [] = []
+nearbys (x:xs)
+  | "nearby" `isPrefixOf` x = map ticket xs
+  | otherwise = nearbys xs
+
+ticket :: String -> Ticket
+ticket [] = []
+ticket (x:xs) = read l:if null r then [] else ticket (tail r)
+  where (l, r) = break (==',') (x:xs)
+
+discard :: [Rule] -> [Ticket] -> [Ticket]
+discard rs ts = [t | t <- ts , valid rs t]
+
+valid :: [Rule] -> [Int] -> Bool
+valid xs ys  = all(\y -> any(\x -> valid' x y) xs) ys
+
+valid' :: Rule -> Int -> Bool
+valid' (x, y) x' = x' >= x && x' <= y
+
+validPositions :: [Field] -> [[Int]] -> Positions
+validPositions fs ys = [(fst y, f) | y <- ys', f <- fs, valid  (snd f) (snd y)]
+   where ys' = zip [0..] ys
+
+remdups :: Positions -> Positions -> Positions
+remdups [] ps' = ps'
+remdups ps ps' = remdups y (ps' ++ xs)
+  where xs = filter(\p ->  length (filter(\p' -> fst p == fst p') ps) == 1) ps
+        ns = map name xs
+        (x, y) = partition (\p -> (name p) `elem` ns) ps
+
+name :: (Position, Field) -> Name
+name (_, (x, _)) = x
+
+fieldValues :: String -> Positions -> Ticket -> [Int]
+fieldValues x ps t = map (\p -> t!!p) ps'
+  where ps' = map fst $ filter(\(x', y) -> x `isPrefixOf` (fst y)) ps
