629f77f04311b55d951af6ac9420948be9c398f2
daniel.cabral <danielc777888@gmail.com>
2021-05-05 18:03:59 +0200

    2020 day 10_2 completed. Divide and conquer

--- 2020/Day10_2.hs
+++ 2020/Day10_2.hs
@@ -4,49 +4,34 @@ main :: IO()
 main = interact solve
 
 solve :: String -> String
-solve xs = show (length as)
-           where xs' = sort $ map (\x -> read x::Int) $ lines xs
-                 xs'' = [0] ++ xs' ++ [last xs' + 3]
-                 rs = remove [0] xs'
-                 as = arrangements xs' rs
-                 bs = filter (\x -> x `notElem` rs) xs'
+solve = show . conquer . divide . adapters
+
+adapters :: String -> [Int]
+adapters = sort . map (\x -> read x::Int) . lines
 
 arrangements :: [Int] -> [Int] -> [[Int]]
 arrangements xs ys = filter (\xs -> valid (sort (xs++bs)))  (subs ys)
    where bs = [0] ++ filter (\x -> x `notElem` ys) xs
 
---candidates :: [Int] -> [[Int]] -> [[Int]]
---candidates [] xs = xs
---candidates xs xss = if valid xs then candidates (generate xs) (xs:xss) else candidates xs
+divide :: [Int] -> ([Int], [[Int]])
+divide xs = (xs, divide' [] xs)
+    where divide' ys (x:y:z:[]) = [ys ++ [x, y, z]]
+          divide' ys (x:y:xs) = if (y - x) == 3 && (length ys) > 1 then [ys ++ [x]] ++ divide' [] (y:xs) else divide' (ys ++ [x]) (y:xs)
 
-generate :: [Int] -> [[Int]]
-generate (x:y:z:xs) = if z - x < 3 then [x:z:xs] ++ generate (x:z:xs) else generate (y:z:xs)
-generate (x:y:[]) = [[]]
-generate _ = [[]]
+conquer :: ([Int], [[Int]]) -> Int
+conquer (xs, xss) = foldl (\acc ys -> if (head ys == 1) then (narrangements 0 xs ys) * acc else (narrangements (head ys - 3) xs ys) * acc) 1 xss
 
+narrangements :: Int -> [Int] -> [Int] -> Int
+narrangements x xs ys = length (arrangements xs (remove [x] ys))
 
 remove :: [Int] -> [Int] ->  [Int]
 remove _ [x] = []
 remove xs (y:ys) = if valid (xs ++ ys) then [y] ++ remove (xs ++ [y]) ys else remove (xs ++ [y]) ys
 
-validate :: ([[Int]], Int) -> [[Int]]
-validate (xss, y) = filter(\xs -> valid ([0] ++ xs ++ [y])) xss
-
 valid :: [Int] -> Bool
 valid (y:[]) = True
 valid (x:y:xs) = if (y - x) > 3 then False else valid (y:xs)
 
-
 subs :: [Int] -> [[Int]]
 subs [] = [[]]
 subs (x:xs) = subs xs ++ map (x:) (subs xs)
-
-subs2 :: [Int] -> [[Int]]        
-subs2 = foldr tstep [[]]
-
-tstep :: Int -> [[Int]] -> [[Int]]
-tstep x ([]:xss) = []:search x [] xss
-                where search x xs [] = [x:xs]
-                      search x xs (ys:xss)
-                        | (head ys - x) >= 3 = ys:search x ys xss
-                        | otherwise = (x:xs):xss
