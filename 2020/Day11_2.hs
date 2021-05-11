72f6a1f337f28f9392cfb52bd1ccf5b4f1852a50
daniel.cabral <danielc777888@gmail.com>
2021-05-11 09:22:10 +0200

    202011_2 completed. Right on!

--- 2020/Day11_2.hs
+++ 2020/Day11_2.hs
@@ -1,6 +1,8 @@
+import qualified Data.Map as Map
+import Data.Maybe
+
 main :: IO()
 main = interact solve
-
 
 type Layout = [Row]
 
@@ -11,46 +13,73 @@ data Seat = Floor Position
           | Occupied Position deriving (Eq, Show)
 
 type Position = (Int, Int)
-type Dims = (Int, Int)
+type SeatMap = Map.Map Position Seat
 
 solve :: String -> String
+--solve = show . layout (0, 0)  . lines
 solve = show . counts . run . layout (0, 0)  . lines
 
-run :: Layout -> Layout
-run xs = run' xs [] 
+dims :: Layout -> (Int, Int)
+dims xss = (w, h)
+    where xs = concat xss
+          h = maximum $ map (fst . position) xs
+          w = maximum $ map (snd . position) xs
 
-run' :: Layout -> Layout -> Layout
-run' xss yss = if xss == yss then xss else run' (seating xss xss []) xss
+run :: Layout -> (Int, Layout)
+run xss = run' xss [] 0 (w, h)
+    where xs = concat xss
+          h = maximum $ map (fst . position) xs
+          w = maximum $ map (snd . position) xs
 
-seating :: Layout -> Layout -> Layout -> Layout
-seating [] xss yss = yss
-seating (xs:xss) yss zss = zss ++ [seatingRow xs yss] ++ seating xss yss [] 
+run' :: Layout -> Layout -> Int -> (Int, Int) -> (Int, Layout)
+run' xss yss x d = if xss == yss then (x, xss) else run' (seating xss xss [] mss d) xss (x+1) d
+    where mss = Map.fromList [(position s, s) | s <- xs]
+          xs = concat xss
 
-seatingRow :: Row -> Layout -> Row
-seatingRow [] _ = []
-seatingRow (x:xs) xss = [seat x ss] ++ seatingRow xs xss
+seating :: Layout -> Layout -> Layout -> SeatMap -> (Int, Int) -> Layout
+seating [] xss yss _ _ = yss
+seating (xs:xss) yss zss sm d = zss ++ [seatingRow xs yss sm d] ++ seating xss yss [] sm d
+
+seatingRow :: Row -> Layout -> SeatMap -> (Int, Int) -> Row
+seatingRow [] _ _ _ = []
+seatingRow (x:xs) xss sm d = (seat x ss) : seatingRow xs xss sm d
   where ps = position x
-        rs = rays ps
-        ss = seats rs xss
+        as = rays ps d
+        ss = seats as sm
 
---get first occupied or empty
-seats :: [[Position]] -> Layout -> [Seat]
-seats ps xss = filter(\s -> position s `elem` ps) $ concat xss
+--gets all seats, for casted ray
+seats :: [[Position]] -> SeatMap -> [Seat]
+seats pss sm = foldr(\ps acc -> let v = firstSeat ps sm in
+                                if isJust v then (fromJust v):acc else acc) [] pss
+
+firstSeat :: [Position] -> SeatMap -> Maybe Seat
+firstSeat [] _ = Nothing
+firstSeat (x:xs) sm = if isJust s && isSeat (fromJust s) then s else firstSeat xs sm
+        where s = Map.lookup x sm
+
+isSeat :: Seat -> Bool
+isSeat (Empty _) = True
+isSeat (Occupied _) = True
+isSeat (Floor _) = False
 
 seat :: Seat -> [Seat] -> Seat
 seat (Empty p) xs = if length (filter occupied xs)  == 0 then Occupied p else Empty p
 seat (Occupied p) xs = if length (filter occupied xs) >= 5 then Empty p else Occupied p
 seat (Floor p) xs = Floor p
-  
-adjacent :: Position -> [Position]
-adjacent (x, y) = filter (\(x, y) -> x >= 0 && y >= 0) xs
-  where xs = [(x-1, y-1), (x-1, y), (x-1, y+1), (x, y-1), (x, y+1), (x+1, y-1), (x+1, y), (x+1, y+1)]
 
---start from top left, and go clockwise
-rays :: Position -> Dims -> [[Position]]
-rays (x, y) (r, c) = [r1, r2, r3, r4, r5, r6, r7, r8]
-  where r1 = 
-  
+--clockwise from north, split into 8 rays order correctly
+rays :: Position -> (Int, Int) -> [[Position]]
+rays (x, y) (w, h) = xs --filter (\(x, y) -> x >= 0 && y >= 0) xs
+  where xs = [r1,r2,r3,r4,r5,r6,r7,r8]
+        r1 = tail $ zip [x,x-1..0] (take (h+1) (repeat y))
+        r2 = tail $ zip [x,x-1..0] [y..w]
+        r3 = tail $ zip (take ((w+1)-y) (repeat x)) [y..w]
+        r4 = tail $ zip [x..h] [y..w]
+        r5 = tail $ zip [x..h] (take ((h+1)-x) (repeat y))
+        r6 = tail $ zip [x..h] [y,y-1..0]
+        r7 = tail $ zip (take (y+1) (repeat x)) [y,y-1..0]
+        r8 = tail $ zip [x,x-1..0] [y,y-1..0]
+
 position :: Seat -> Position
 position (Floor p) = p
 position (Empty p) = p
@@ -68,12 +97,9 @@ row (r, c) (x:xs)
   | x == '#' = [Occupied (r, c)] ++ row (r, c+1) xs
   | otherwise = error "unrecognized seating"
 
-counts :: Layout -> Int
-counts = length . filter occupied . concat
+counts :: (Int, Layout) -> Int
+counts = length . filter occupied . concat . snd
 
 occupied :: Seat -> Bool
 occupied (Occupied _) = True
 occupied _ = False
-
-dims :: Layout -> Dims
-dims xss = (length xss, length (head xss))
