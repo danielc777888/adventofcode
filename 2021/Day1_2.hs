fb7a15710291adf5058636f39424fd46fba1b720
danielc777888 <daniel777888@gmail.com>
2021-12-01 10:56:04 +0200

    2021-1 ok

---
+++ 2021/Day1_2.hs
@@ -0,0 +1,14 @@
+
+main :: IO()
+main = interact solve
+
+solve :: String -> String
+solve = show . countInc 0 . sums . map read . lines
+
+countInc :: Int -> [Int] -> Int
+countInc n (y:[]) = n
+countInc n (x:y:xs) = if y > x then countInc (n+1) (y:xs) else countInc n (y:xs)
+
+sums :: [Int] -> [Int]
+sums (x:y:z:xs) = (sum [x,y,z]) : sums (y:z:xs)
+sums _ = []
