2ebe6b8c9ad0413365fe55e8c8ef0b2961e405ed
danielc777888 <daniel777888@gmail.com>
2021-12-07 09:02:08 +0200

    202106(ok,ok)

---
+++ 2021/Day6_1.hs
@@ -0,0 +1,24 @@
+
+main :: IO()
+main = interact solve
+
+solve :: String -> String
+solve = show . length . spawn 80 . initialState
+
+initialState :: String -> [Int]
+initialState [] = []
+initialState xs = read ((takeWhile (/= ',') xs)) : initialState ys'
+    where ys = dropWhile (/= ',') xs
+          ys' = if null ys then [] else tail ys
+
+spawn :: Int -> [Int] -> [Int]
+spawn 0 xs = xs
+spawn n xs = spawn (n-1) (step xs)
+
+step :: [Int] -> [Int]
+step [] = []
+step (x:xs)
+    | x == 0 = 6:8:step xs
+    | otherwise = (x-1):step xs
+
+
