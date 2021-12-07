2cb571dbe489c9b4fea74ec0edbdfaa61a30b68b
danielc777888 <daniel777888@gmail.com>
2021-12-07 09:32:26 +0200

    202107(ok,ok)

---
+++ 2021/Day7_2.hs
@@ -0,0 +1,15 @@
+
+main :: IO()
+main = interact solve
+
+solve :: String -> String
+solve = show . minimum . costs . map read . split ','
+
+costs :: [Int] -> [Int]
+costs xs = map (cost xs) [0..maximum xs]
+
+cost :: [Int] -> Int -> Int
+cost xs x = sum $ map (\y -> sum [0..abs(y - x)]) xs
+
+split :: Char -> String -> [String]
+split c = words . map (\x -> if x == c then ' ' else x)
