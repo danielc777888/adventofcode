209e64c9081dc4084b787b8d72df88ddfd7616f2
danielc777888 <daniel777888@gmail.com>
2021-12-09 15:01:06 +0200

    202108(ok,ok)

---
+++ 2021/Day8_1.hs
@@ -0,0 +1,18 @@
+
+type Digit = String
+
+main :: IO()
+main = interact solve
+
+solve :: String -> String
+solve = show . counts . concat . map digits . lines
+
+digits :: String -> [Digit]
+digits = words . drop 2 . dropWhile (/= '|')
+
+counts :: [Digit] -> Int
+counts [] = 0
+counts (x:xs)
+  | l == 2 || l == 3 || l == 7 || l == 4 = 1 + counts xs
+  | otherwise = counts xs
+    where l = length x
