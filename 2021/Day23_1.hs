3ce8c733f5584d7766eb26d84e35b024557aa255
danielc777888 <daniel777888@gmail.com>
2021-12-23 17:43:49 +0200

    202123(nok,nok)

---
+++ 2021/Day23_1.hs
@@ -0,0 +1,29 @@
+import Data.Array
+
+type Burrow = Array Int (Maybe Amphipod)
+data Amphipod = Amber
+    | Bronze
+    | Copper
+    | Desert deriving Show
+
+main :: IO()
+main = interact solve
+
+solve :: String -> String
+solve = show . minimum . moves . burrow . lines
+
+burrow :: [String] -> Burrow
+burrow = undefined
+
+moves :: Burrow -> [Int]
+moves = undefined
+
+organized :: Burrow -> Bool
+organized = undefined
+
+energy :: Amphipod -> Int
+energy Amber = 1
+energy Bronze = 10
+energy Copper = 100
+energy Desert = 1000
+
