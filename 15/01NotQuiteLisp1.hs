6406d84531bf27745206e9b50dab331dfe48ac50
danielc777888 <daniel777888@gmail.com>
2022-05-16 11:22:35 +0200

    15-01NotQuiteLisp1-Accepted

--- 15/01NotQuiteLisp1.hs
+++ 15/01NotQuiteLisp1.hs
@@ -1,6 +1,13 @@
+ex1 = ")())())"
+
 solve :: String -> Int
 solve [] = 0
-solve (x:xs) = if x == '(' then 1 + solve xs else solve xs -1 
+solve (x : xs) = if x == '(' then 1 + solve xs else solve xs -1
 
-main :: IO()
-main = interact (show . solve)
+main :: IO ()
+main = interact (show . solve . head . lines)
+
+{-- main = do
+let a = solve ex1
+putStrLn (show a)
+--}
