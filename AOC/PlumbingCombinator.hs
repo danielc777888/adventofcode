8383569ea6b94386d803b7421b0576afd852332e
danielc777888 <daniel777888@gmail.com>
2022-09-08 21:05:42 +0200

    1516-Accepted

---
+++ AOC/PlumbingCombinator.hs
@@ -0,0 +1,7 @@
+module AOC.PlumbingCombinator
+  ( fork
+  )
+where
+
+fork :: (a -> b, a -> c) -> a -> (b, c)
+fork (f, g) x = (f x, g x)
---
+++ template.hs
@@ -0,0 +1,10 @@
+import AOC.PlumbingCombinator (fork)
+
+main :: IO()
+main = interact $ show . fork (solve1, solve2)
+
+solve1 :: String -> String
+solve1 = id
+
+solve2 :: String -> String
+solve2 = id
