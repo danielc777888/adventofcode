4720bf6b5dfb2f760230cd35b4bbe4f6da1145d4
daniel-dev <danielc777888@gmail.com>
2024-01-09 13:25:24 +0200

    23/08 used foldl

--- 23/08-2.hs
+++ 23/08-2.hs
@@ -1,3 +1,7 @@
+-- TODO
+-- try using a list of binary trees and traverse
+
+import Data.List
 import qualified Data.Map as M
 import Data.Maybe
 
@@ -19,14 +23,18 @@ network xs = (head xs, m)
 
 
 run :: Char -> Char -> Network -> Int
-run s e (xs, m) = run' ss e m (cycle xs) 0
-  where ss = filter (\x -> last x == s) (M.keys m)
+run s e (xs, m) = run' (ss,s) e m (cycle xs) 0
+  where ss = filter (\[_,_,c] -> c == s) (M.keys m)
 
-run' :: [String] -> Char -> M.Map String (String, String) -> String -> Int -> Int
-run' ss d m (x:xs) n
+run' :: ([String], Char) -> Char -> M.Map String (String, String) -> String -> Int -> Int
+run' (ss,c) d m (x:xs) n
   | n == 10000000 = n
-  | all (\s -> last s == d) ss = n
+  | c == d = n
   | otherwise = run' ss' d m xs (n+1)
-    where ss' = map(\s -> let (l,r) = fromJust (M.lookup s m) in
-                       if x == 'L' then l else r) ss
+    where ss' = foldl' (\(ys, d') s ->
+                         let (l,r) = fromJust (M.lookup s m)
+                             s'@[_,_,c] = if x == 'L' then l else r
+                         in (s':ys, if d' == 'z' || c == d' then d' else 'z')) ([],d) ss
+--    where ss' = map(\s -> let (l,r) = fromJust (M.lookup s m) in
+  --                     if x == 'L' then l else r) ss
 run' _ _ _ [] n = n
