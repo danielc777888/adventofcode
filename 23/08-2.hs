ec0cff2e32c420f19d4be3c5ee5327b4d346a9a2
daniel-dev <danielc777888@gmail.com>
2024-01-12 18:02:15 +0200

    23/08-2 tried arrays

--- 23/08-2.hs
+++ 23/08-2.hs
@@ -1,17 +1,17 @@
 -- TODO
--- get counts of when condition met
---
+-- store items and l,r in array
+-- use array to access elems, O(1) instead of O(log n)
 
+import Data.Array
 import Data.List
 import qualified Data.Map as M
 import Data.Maybe
 
-
-data BinTree a = Leaf a | Fork a (BinTree a) (BinTree a) deriving Show
+data BinTree a = Leaf a | Fork a (BinTree a) (BinTree a) deriving (Eq,Show)
 
 type Node = (String, Char)
-
-type Network = (String, M.Map String (String, String))
+type Paths = (Int, Int)
+type Network = (String, Array Int (Char, Paths))
 
 main :: IO ()
 main = interact solve
@@ -21,25 +21,46 @@ solve :: String -> String
 solve = show . run 'A' 'Z' . network . lines
 
 network :: [String] -> Network
-network xs = (head xs, m)
-  where m = M.fromList $ map node $ drop 2 xs
+network xs = (head xs, ar)
+  where xs' = map node (drop 2 xs)
+        m = M.fromList (zip (map fst xs') [0..])
+        ar = array (0, length xs' - 1) (zipWith (\i (e, (l,r)) -> (i, (last e, (findNodeIdx m l, findNodeIdx m r)))) [0..] xs')
         node s = case (words (filter (\x -> x /= '=' && x /= ',' && x /= '(' && x /= ')') s)) of
-          [e,l,r] -> (e, (l, r))
-          e       -> error ("invalid node pattern: " ++ show e)
+                   [e,l,r] -> (e, (l, r))
+                   e       -> error ("invalid node pattern: " ++ show e)
+
+findNodeIdx :: M.Map String Int -> String -> Int
+findNodeIdx m s = fromJust (M.lookup s m)
+
+run :: Char -> Char -> Network -> Int
+run s e (xs, ar) = run' ar is e (cycle xs) 0
+  where is = map fst $ filter ( (s==) . fst . snd) $ assocs ar
 
 
+run' :: Array Int (Char, Paths) -> [Int] -> Char -> [Char] -> Int -> Int
+run' arr ids e (x:xs) n
+  | n == 100000000 || b = n
+  | otherwise = run' arr ids' e xs (n+1)
+  where nodes = map (arr!) ids
+        --ids' = map (\(_, ps) -> if x == 'L' then fst ps else snd ps) nodes
+        (ids', b) = foldr (\i (ys,b) -> let (c, (l,r)) = arr!i in
+                              (if x == 'L' then l:ys else r:ys, if not b || c /= e then False else b)) ([], True) ids
+
+{--
 run :: Char -> Char -> Network -> Int
 run s e (xs, m) = nodes ts (cycle xs) 0 --run' (ss,s) e m (cycle xs) 0
   where ss = filter (\[_,_,c] -> c == s) (M.keys m)
-        ts = map (build m 100000000) ss
+        ts = map (build m M.empty) ss
 
-build :: M.Map String (String, String) -> Int -> String -> BinTree Node
-build m n s
+build :: M.Map String (String, String) -> M.Map String (BinTree Node) -> String -> BinTree Node
+build m cs s
   -- | (last s) == 'Z' = Leaf (s, 'Z')
-  | (l,r) == (s,s) || n == 0 = Leaf (s, last s)
-  | otherwise = Fork (s, last s) (build m (n-1) l) (build m (n-1) r)
+  | (l,r) == (s,s) = Leaf (s, last s)
+  | otherwise = if M.member s cs then fromJust (M.lookup s cs) else f
   where (l,r) = fromJust (M.lookup s m)
-
+        f = Fork (s, last s) lt rt
+        lt = build m (M.insert s f cs) l
+        rt = build m (M.insert s f cs) r
 
 nodes :: [BinTree Node] -> String -> Int -> Int
 nodes ts (x:xs) !n
@@ -68,16 +89,4 @@ run' (ss,c) d m (x:xs) n
 --    where ss' = map(\s -> let (l,r) = fromJust (M.lookup s m) in
   --                     if x == 'L' then l else r) ss
 run' _ _ _ [] n = n
-
-
-
-{--
-
-          Fork("11A",'A')
-      (Fork ("11B",'B')
-(Leaf ("XXX",'X'))(Leaf ("11Z",'Z'))) (Leaf ("XXX",'X'))
-
-
-
-Fork ("22A",'A') (Fork ("22B",'B') (Fork ("22C",'C') (Leaf ("22Z",'Z')) (Leaf ("22Z",'Z'))) (Fork ("22C",'C') (Leaf ("22Z",'Z')) (Leaf ("22Z",'Z')))) (Leaf ("XXX",'X'))
 --}
