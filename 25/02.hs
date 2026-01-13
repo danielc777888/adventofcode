4677c48c5ac4530a7d860f06fc610726baa0395c
daniel-dev <danielc777888@gmail.com>
2026-01-13 22:21:45 +0200

    25022P

--- 25/02.hs
+++ 25/02.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.List
 import qualified Data.Text as T
 import qualified Data.Text.IO as TI
 
@@ -12,20 +13,19 @@ main = do
   let ranges = parse contents
   print ranges
   putStrLn ("Part 1: " <> show (part1 ranges))
-  putStrLn ("Part 2: " <> show (part2 contents))
-
+  putStrLn ("Part 2: " <> show (part2 ranges))
 
 parse :: T.Text -> [Range]
 parse = map (\y -> let [f, t] = T.splitOn "-" y
                        in (read (T.unpack f), read (T.unpack t))) .  T.splitOn ","
 
--- PASS
+-- 24747430309
 part1 :: [Range] -> Integer
 part1 = sum . concatMap (\(x, y) -> filter invalid [x .. y])
 
--- FAIL
-part2 :: T.Text -> T.Text
-part2 = id
+-- 30962646823
+part2 :: [Range] -> Integer
+part2 = sum . concatMap (\(x, y) -> filter invalid' [x .. y])
 
 invalid :: Integer -> Bool
 invalid x = xs == ys
@@ -34,3 +34,13 @@ invalid x = xs == ys
     mid = length x' `div` 2
     (xs, ys) = splitAt mid x'
 
+invalid' :: Integer -> Bool
+invalid' x = any (\c -> length c == 1) cs
+  where
+    x' = show x 
+    mid = length x' `div` 2
+    cs = map (nub . chunks x') [1..mid]
+
+chunks :: [Char] -> Int -> [String]
+chunks [] _ = []
+chunks xs c = take c xs : chunks (drop c xs) c
