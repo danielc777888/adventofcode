080501a0565b4d5fff6d543c241b4337e905cfe3
danielc777888 <danielc777888@gmail.com>
2026-02-24 11:50:06 +0200

    25032F

--- 25/03.hs
+++ 25/03.hs
@@ -6,17 +6,20 @@ main = do
   putStrLn "2025 -- Day 3 -- Lobby"
   contents <- getContents
   let banks = lines contents
-  putStrLn ("Part 1: " <> show (part1 banks))
+  -- putStrLn ("Part 1: " <> show (part1 banks))
   putStrLn ("Part 2: " <> show (part2 banks))
 
 -- 17524
 part1 :: [String] -> Int
 part1 = sum . map largestJoltage
 
--- 
-part2 :: [String] -> [Integer]
--- part2 = sum . map (mss 12)
-part2 = map (mss 12)
+-- take tail lenth of 12, this in the min
+-- take first number look for number larger than it, go left
+-- if found replace (not indx of using number)
+-- if not found go to next number in final set
+part2 :: [String] -> Integer
+part2 = sum . map largestJoltage'
+-- part2 = map largestJoltage'
 
 mss :: Int -> String -> Integer
 mss b = read . maximum . filter (short b) . segments
@@ -41,4 +44,23 @@ largestJoltage xs = read [d1 ,d2]
   where d1 = maximum (init xs)
         (_, xs') = break (== d1) xs 
         d2 = maximum (tail xs')
-        
+
+largestJoltage' :: String -> Integer
+largestJoltage' xs = read mx
+  where (ys, zs) = splitAt (length xs - 12) xs
+        mx = max (take 12 xs) (findLargest ys zs)
+
+findLargest :: String -> String -> String
+findLargest [] ys = ys
+findLargest _ [] = []
+findLargest xs (y:ys) 
+  | y > mx  = (y : ys) 
+  | y == mx = mx : findLargest xs ys
+  | otherwise = mx : findLargest xs' ys
+  where mx = maximum xs
+        xs' = tail $ dropWhile (\x -> x < mx) xs
+
+
+findMax :: [(Int, Char)] -> (Int, Char)
+findMax = maximumBy (compare `on` snd)
+
